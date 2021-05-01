2fb11ee0b5e7ca07928bae788be78a480ef3efa1
zwj-tina <53886378+zwj-tina@users.noreply.github.com>
2021-05-01 15:13:28 +0800

    Update pre-process.py

--- pre-process.py
+++ pre-process.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 
 # tcga
-data = pd.read_csv("../tcga_RSEM_gene_tpm", sep = "\t")# gtex 
-gene_map = pd.read_csv("../probeMap%2Fgencode.v23.annotation.gene.probemap", sep = "\t")
+data = pd.read_csv("./tcga_RSEM_gene_tpm", sep = "\t")# gtex 
+gene_map = pd.read_csv("./probeMap%2Fgencode.v23.annotation.gene.probemap", sep = "\t")
 ids = gene_map["id"].tolist()
 gene = gene_map["gene"].tolist()
 geneSymbol = []
@@ -16,9 +16,9 @@ indexes = data["geneSymbol"].tolist()
 data = data.iloc[:,1:-1]
 data.index = indexes
 data_tpm = 2 ** data  - 0.001
-data_tpm.to_csv("../tcgaTpmGenes.csv",index_label = "gene")
+data_tpm.to_csv("./tcgaTpmGenes.csv",index_label = "gene")
 #gtex
-data = pd.read_csv("../gtex_RSEM_gene_tpm", sep = "\t")# gtex 
+data = pd.read_csv("./gtex_RSEM_gene_tpm", sep = "\t")# gtex 
 ids = gene_map["id"].tolist()
 gene = gene_map["gene"].tolist()
 geneSymbol = []
@@ -31,4 +31,4 @@ indexes = data["geneSymbol"].tolist()
 data = data.iloc[:,1:-1]
 data.index = indexes
 data_tpm = 2 ** data  - 0.001
-data_tpm.to_csv("../gtexTpmGenes.csv",index_label = "gene")
+data_tpm.to_csv("./gtexTpmGenes.csv",index_label = "gene")
