4ee7d97b79e89b37b0a373d066b70c162d9ddac7
zwj-tina <53886378+zwj-tina@users.noreply.github.com>
2021-05-01 14:51:17 +0800

    Update quantiseq.R

--- quantiseq.R
+++ quantiseq.R
@@ -1,9 +1,10 @@
 library(immunedeconv)
+#gtex
 mixture_file <- read.csv("../gtexTpmGenes.csv")
 rownames(mixture_file) <- mixture_file$gene
 mixture_file <- mixture_file[,-1]
 gtex <- immunedeconv::deconvolute(mixture_file, "quantiseq")
-
+#tcga
 mixture_file <- read.csv("../tcgaTpmGenes.csv")
 rownames(mixture_file) <- mixture_file$gene
 mixture_file <- mixture_file[,-1]
