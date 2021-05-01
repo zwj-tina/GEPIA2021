a8b9005cc7cb0e5a3054b3d8d2c4e91eacb544cf
zwj-tina <53886378+zwj-tina@users.noreply.github.com>
2021-05-01 14:50:56 +0800

    Update EPIC.R

--- EPIC.R
+++ EPIC.R
@@ -1,9 +1,10 @@
 library(EPIC)
+#gtex
 bulkSamplesMatrix <- read.csv("../gtexTpmGenes.csv")
 rownames(bulkSamplesMatrix) <- bulkSamplesMatrix$gene
 bulkSamplesMatrix <- bulkSamplesMatrix[,-1]
 gtex <- EPIC(bulk = bulkSamplesMatrix)
-
+#tcga
 bulkSamplesMatrix <- read.csv("../tcgaTpmGenes.csv")
 rownames(bulkSamplesMatrix) <- bulkSamplesMatrix$gene
 bulkSamplesMatrix <- bulkSamplesMatrix[,-1]
