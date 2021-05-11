6cdd7353bb121e9b292d2de84f26f1133533294f
zwj-tina <53886378+zwj-tina@users.noreply.github.com>
2021-05-11 11:16:54 +0800

    Update CIBERSORT_process.R

--- CIBERSORT_process.R
+++ CIBERSORT_process.R
@@ -7,10 +7,10 @@ mixture_file2 <- read.csv("./gtexTpmGenes.csv")
 rownames(mixture_file2) <- mixture_file2$gene
 mixture_file2 <- mixture_file2[,-1]
 LM_GTEX_part <- CIBERSORT(sig_matrix = sig_matrix, mixture_file = mixture_file2, perm=0, QN=FALSE, absolute=TRUE, abs_method='sig.score')
-write.csv(LM_GTEX, "./gtex_LM_propotion_part.csv")
+write.csv(LM_GTEX, "./gtex_LM_propotion.csv")
 
 mixture_file <- read.csv("./tcgaTpmGenes.csv")
 rownames(mixture_file) <- mixture_file$gene
 mixture_file <- mixture_file[,-1]
 LM_TCGA <- CIBERSORT(sig_matrix = sig_matrix, mixture_file = mixture_file, perm=0, QN=FALSE, absolute=TRUE, abs_method='sig.score')
-write.csv(LM_TCGA, "./tcga_LM_propotion_all.csv")
+write.csv(LM_TCGA, "./tcga_LM_propotion.csv")
