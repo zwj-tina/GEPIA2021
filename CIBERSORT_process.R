3674d7df54c0080166f4fc00b849f23da5d38139
zwj-tina <53886378+zwj-tina@users.noreply.github.com>
2021-05-11 13:35:58 +0800

    Update CIBERSORT_process.R

--- CIBERSORT_process.R
+++ CIBERSORT_process.R
@@ -1,3 +1,45 @@
+# CIBERSORT R script v1.04 (last updated 10-24-2016)
+# Note: Signature matrix construction is not currently available; use java version for full functionality.
+# Author: Aaron M. Newman, Stanford University (amnewman@stanford.edu)
+# Requirements:
+#       R v3.0 or later. (dependencies below might not work properly with earlier versions)
+#       install.packages('e1071')
+#       install.pacakges('parallel')
+#       install.packages('preprocessCore')
+#       if preprocessCore is not available in the repositories you have selected, run the following:
+#           source("http://bioconductor.org/biocLite.R")
+#           biocLite("preprocessCore")
+# Windows users using the R GUI may need to Run as Administrator to install or update packages.
+# This script uses 3 parallel processes.  Since Windows does not support forking, this script will run
+# single-threaded in Windows.
+#
+# Usage:
+#       Navigate to directory containing R script
+#
+#   In R:
+#       source('CIBERSORT.R')
+#       results <- CIBERSORT('sig_matrix_file.txt','mixture_file.txt', perm, QN, absolute, abs_method)
+#
+#       Options:
+#       i)   perm = No. permutations; set to >=100 to calculate p-values (default = 0)
+#       ii)  QN = Quantile normalization of input mixture (default = TRUE)
+#       iii) absolute = Run CIBERSORT in absolute mode (default = FALSE)
+#               - note that cell subsets will be scaled by their absolute levels and will not be
+#                 represented as fractions (to derive the default output, normalize absolute
+#                 levels such that they sum to 1 for each mixture sample)
+#               - the sum of all cell subsets in each mixture sample will be added to the ouput
+#                 ('Absolute score'). If LM22 is used, this score will capture total immune content.
+#       iv)  abs_method = if absolute is set to TRUE, choose method: 'no.sumto1' or 'sig.score'
+#               - sig.score = for each mixture sample, define S as the median expression
+#                 level of all genes in the signature matrix divided by the median expression
+#                 level of all genes in the mixture. Multiple cell subset fractions by S.
+#               - no.sumto1 = remove sum to 1 constraint
+#
+# Input: signature matrix and mixture file, formatted as specified at http://cibersort.stanford.edu/tutorial.php
+# Output: matrix object containing all results and tabular data written to disk 'CIBERSORT-Results.txt'
+# License: http://cibersort.stanford.edu/CIBERSORT_License.txt
+
+
 source('CIBERSORT.R')
 sig_matrix <- read.csv("./LM22.txt", sep = "\t")
 rownames(sig_matrix)  <- sig_matrix$Gene.symbol
