002890276088df9832c5bf52cce968e14d93fc74
zwj-tina <53886378+zwj-tina@users.noreply.github.com>
2021-05-11 15:31:09 +0800

    Update pre-process.py

--- pre-process.py
+++ pre-process.py
@@ -16,7 +16,16 @@ indexes = data["geneSymbol"].tolist()
 data = data.iloc[:,1:-1]
 data.index = indexes
 data_tpm = 2 ** data  - 0.001
-data_tpm.to_csv("./tcgaTpmGenes.csv",index_label = "gene")
+protein =  pd.read_csv("./protein.txt", sep = "\t")
+gene = protein["Symbol"].tolist()
+gene_need = [i for i in gene if i in data_tpm.index.tolist()]
+data_tpm = data_tpm.loc[gene_need,:]
+col_sum = data_tpm.sum(axis = 0)
+ax = 1e6 / col_sum
+data_tpm = data_tpm * ax
+data_tpm.to_csv("./tcgaTpm.csv",index_label = "gene")
+
+
 #gtex
 data = pd.read_csv("./gtex_RSEM_gene_tpm", sep = "\t")# gtex 
 ids = gene_map["id"].tolist()
@@ -31,4 +40,11 @@ indexes = data["geneSymbol"].tolist()
 data = data.iloc[:,1:-1]
 data.index = indexes
 data_tpm = 2 ** data  - 0.001
-data_tpm.to_csv("./gtexTpmGenes.csv",index_label = "gene")
+protein =  pd.read_csv("./protein.txt", sep = "\t")
+gene = protein["Symbol"].tolist()
+gene_need = [i for i in gene if i in data_tpm.index.tolist()]
+data_tpm = data_tpm.loc[gene_need,:]
+col_sum = data_tpm.sum(axis = 0)
+ax = 1e6 / col_sum
+data_tpm = data_tpm * ax
+data_tpm.to_csv("./gtexTpm.csv",index_label = "gene")
